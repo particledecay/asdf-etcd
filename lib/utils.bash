b50ee3259d1a8d9f26bca844ea0e32d21f37299f
Joey Espinosa <jlouis.espinosa@gmail.com>
2023-11-17 09:29:40 -0500

    fix: revert "force etcdctl to be executable"
    This reverts commit 7cd491b15db60273270a96a791f8697658ab46e0.

--- lib/utils.bash
+++ lib/utils.bash
@@ -64,8 +64,7 @@ install_version() {
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || chmod +x "$install_path/$tool_cmd"
-		# test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
